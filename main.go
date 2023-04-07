601a45a29a1cef13779fb655c3b1b26c7cee398f
PiccoloMondoC <119958916+PiccoloMondoC@users.noreply.github.com>
2023-04-07 10:57:55 -0500

    Add files via upload

---
+++ main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"google.golang.org/api/option"
+	"google.golang.org/api/pubsub/v1"
+	"google.golang.org/grpc"
+)
+
+// Config represents the configuration settings for the project.
+type Config struct {
+	ProjectID      string
+	PubsubTopic    string
+	PubsubSub      string
+	PubsubEndpoint string
+}
+
+// LoadConfig loads the configuration settings from environment variables.
+func LoadConfig() (*Config, error) {
+	cfg := &Config{
+		ProjectID:      os.Getenv("PUBSUB_MANAGER_PROJECT_ID"),
+		PubsubTopic:    os.Getenv("PUBSUB_MANAGER_TOPIC"),
+		PubsubSub:      os.Getenv("PUBSUB_MANAGER_SUBSCRIPTION"),
+		PubsubEndpoint: os.Getenv("PUBSUB_MANAGER_ENDPOINT"),
+	}
+
+	return cfg, nil
+}
+
+// PubSubManager represents the Pub/Sub Manager instance.
+type PubSubManager struct {
+	Config     *Config
+	PubSub     *pubsub.Service
+	HttpServer *http.Server
+}
+
+// NewPubSubManager creates a new Pub/Sub Manager instance.
+func NewPubSubManager(cfg *Config) (*PubSubManager, error) {
+	ctx := context.Background()
+	creds, err := grpc.GoogleDefaultCredentials()
+	if err != nil {
+		log.Fatalf("Failed to get gRPC credentials: %v", err)
+	}
+
+	// Authenticate with the service account key file
+	opts := option.WithCredentialsFile(cfg.ServiceAccount)
+	pubSub, err := pubsub.NewService(ctx, opts)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to create Pub/Sub client: %v", err)
+	}
+
+	// Create the HTTP server and router
+	router := mux.NewRouter()
+	server := &http.Server{
+		Addr:    cfg.PubsubEndpoint,
+		Handler: router,
+	}
+
+	// Create the PubSub Manager instance
+	return &PubSubManager{
+		Config:     cfg,
+		PubSub:     pubSub,
+		HttpServer: server,
+	}, nil
+}
+
+// Start starts the Pub/Sub Manager instance.
+func (mgr *PubSubManager) Start() error {
+	log.Printf("Starting Pub/Sub Manager on port %s", mgr.HttpServer.Addr)
+	return mgr.HttpServer.ListenAndServe()
+}
+
+// Stop stops the Pub/Sub Manager instance.
+func (mgr *PubSubManager) Stop() error {
+	log.Printf("Stopping Pub/Sub Manager")
+	return mgr.HttpServer.Shutdown(context.Background())
+}
+
+func main() {
+	// Load the PubSub Manager configuration from environment variables
+	cfg, err := LoadConfig()
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+	}
+
+	// Create a new PubSub Manager instance
+	mgr, err := NewPubSubManager(cfg)
+	if err != nil {
+		log.Fatalf("Failed to create Pub/Sub Manager: %v", err)
+	}
+
+	// Start the Pub/Sub Manager instance
+	if err := mgr.Start(); err != nil {
+		log.Fatalf("Failed to start Pub/Sub Manager: %v", err)
+	}
+}
